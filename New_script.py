eb87387145734b861d37de40506df5a50f58c3fc
Maverick Wong <mvrck.wong@gmail.com>
2023-04-05 08:33:04 +0800

    Update New_script.py

--- New_script.py
+++ New_script.py
@@ -2,5 +2,9 @@
 
 fake_array = [1,2,3,4,5]
 
-for i,f, in enumerate(fake_array):
-    print('Iteration: ' + str(i) + ', Value: ' + str(f))
+def main() -> None:
+    for i,f, in enumerate(fake_array):
+        print('Iteration: ' + str(i) + ', Value: ' + str(f))
+        
+
+main()
